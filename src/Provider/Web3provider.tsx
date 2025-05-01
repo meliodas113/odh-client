ff7eaf77b36de0e94f5998fa95f3258f2131b06e
meliodas113 <anupam.fas27@gmail.com>
2025-05-02 00:37:30 +0530

    added fix for admin

--- src/Provider/Web3provider.tsx
+++ src/Provider/Web3provider.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 import { WagmiProvider, createConfig, http } from "wagmi";
-import {etherlink} from 'wagmi/chains'
+import { etherlink } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import { ReactNode } from "react";
@@ -9,26 +9,30 @@ export const config = createConfig(
   getDefaultConfig({
     chains: [etherlink],
     transports: {
-      [etherlink.id]:http(
+      [etherlink.id]: http(
         "https://node.mainnet.etherlink.com"
         // "https://rpc.api.moonbeam.network"
-      )
+      ),
     },
     walletConnectProjectId: "c0d339ed8343fed3a39dd6795c2244b8",
     appName: "BaseForeCast",
-  }),
+  })
 );
 
 const queryClient = new QueryClient();
 
-export const Web3Provider = ({ children }:{ children: ReactNode }) => {
+export const Web3Provider = ({ children }: { children: ReactNode }) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider 
-        theme="midnight" customTheme={{
-    "--ck-font-family": '"Comic Sans MS", "Comic Sans", cursive',
-  }} >{children}</ConnectKitProvider>
+        <ConnectKitProvider
+          theme="midnight"
+          customTheme={{
+            "--ck-font-family": '"Comic Sans MS", "Comic Sans", cursive',
+          }}
+        >
+          {children}
+        </ConnectKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
